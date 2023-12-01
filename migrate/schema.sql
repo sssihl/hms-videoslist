7534e76cebe0e97e7b8eb7e56d1e09dd9a886ec6
Hursh Gupta <hurshgupta1@gmail.com>
2023-12-01 15:55:27 +0530

    connected db, env vars

---
+++ migrate/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `dbname`.`rec_save_videos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `patient_id` INT NOT NULL,
+  `video_type` VARCHAR(10) NOT NULL,
+  `filename` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `filename_UNIQUE` (`filename` ASC) VISIBLE)
+COMMENT = 'for the rec_save mini project (don\'t use dashes \"-\" in names)';
