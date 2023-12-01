34ef2aa53a310a3084dad1fbcb28ad1199cded73
Hursh Gupta <hurshgupta1@gmail.com>
2023-12-01 16:47:59 +0530

    patients table added

---
+++ migrations/patients_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `mini_projects`.`rec_save_patients` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `dob` DATE NOT NULL,
+  `sex` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`id`));
